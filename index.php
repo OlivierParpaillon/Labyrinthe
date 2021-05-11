ed179975a606fd925230c479581aff551ae1acb6
OlivierParpaillon <olivier.parpaillon@campus.academy>
2021-05-11 15:49:11 +0200

    modif

--- index.php
+++ index.php
@@ -5,61 +5,32 @@
     <title>Jeu du labyrinthe</title>
     <meta charset="utf-8">
     <style>
-        .mur {
-            background-color: black;
-        }
+       body{
+           background-image: url(ressources/lab.jpg);
+       } 
 
-        .chemin {
-            background-color: antiquewhite;
-        }
+       .form{
+           margin: 0 auto;
+           padding: 150px;
+           width: 280px;
+       }
 
-        td {
-            width: 20px;
-            height: 20px;
-        }
     </style>
+
 </head>
 
-<body> 
+<body>
 
-    <?php       //Ouvrir un fichier
-        $file = fopen('ressources/labyrinthe.txt', 'r+');
+<div class="form">
+    <h1>Bienvenue dans le jeu du labyrinthe !</h1>
+    <h3>Pour commencer, veuillez choisir un pseudo :</h3>
 
-        $tablab = [];
-        while(!feof($file))  {
-            $ligne = fgets($file);
-            $tablab[] = explode(" ", $ligne);
-            echo $ligne . "<br />";
-        }     
-        fclose($file);
-    ?>
-
-    <table> <!-- lire un fichier -->
-        <?php foreach( $tablab as $ligne) :?>
-            <tr>
-                <?php foreach( $ligne as $case) :?>
-                    <?php if($case == 0) :?>
-                        <td class="mur">
-                    <?php endif; ?>
-                    <?php if ($case == 1) :?>
-                        <td class="chemin">
-                    <?php endif; ?>
-                    </td>
-                <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>    
-    </table>
-
-
-
-<!-- formulaire pseudo -->   
-    <form action="" method="POST"> 
+    <!-- formulaire pseudo -->   
+    <form action="game.php" method="POST"> 
         <div>
             <input type="text" name="pseudo">
             <button type="submit" name="surname">Selectionner ce pseudo</button>
         </div>
     </form>
-
-</body> 
-
-</html>
+</div>
+</body>
